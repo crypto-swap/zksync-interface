a0e4a5fa9bdb5b9e602e4775a3fe20d6175dcf84
Nathan Lou <natloudev@gmail.com>
2022-03-25 17:15:54 -0700

    tests smaller

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            CryptoSwap baby haahhhaaaahahahaahh!
+            CryptoSwap!
           </a>
         </h1>
 
