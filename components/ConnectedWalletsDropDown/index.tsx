4d27c523848d71d4445234b3b9474deee5fd11bc
Katsuya Wakabayashi <wakabayashi@ucla.edu>
2022-03-27 11:28:49 -0700

    Add separable navbar

---
+++ components/ConnectedWalletsDropDown/index.tsx
@@ -0,0 +1,71 @@
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+import { Wallet } from '../SwapMenu/SwapInput'
+
+interface ConnectedWalletsDropDownProps {
+  wallets: Wallet[]
+}
+
+export default function ConnectedWalletsDropDown({
+  wallets,
+}: ConnectedWalletsDropDownProps) {
+  const [selected, setSelected] = useState<Wallet>()
+
+  return (
+    <div className="w-32 text-white sm:block sm:w-44">
+      <Listbox value={selected} onChange={setSelected}>
+        <div className="relative">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-[#7408F8] py-2.5 pl-3 pr-10 text-left text-xs text-button-blue shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-blue-500">
+            <span className="block truncate">
+              {selected ? selected.name : 'Connected Wallets'}
+            </span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <SelectorIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-xs shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ">
+              {wallets.map((wallet, walletIdx) => (
+                <Listbox.Option
+                  key={walletIdx}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? 'bg-blue-100 text-white' : 'text-gray-900'
+                    }`
+                  }
+                  value={wallet}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {wallet.name}
+                      </span>
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-button-blue">
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  )
+}
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,68 +2,84 @@ import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import ConnectedWalletsDropDown from '../ConnectedWalletsDropDown'
+import NetworksDropDown from '../NetworksDropDown'
+import { Wallet } from '../SwapMenu/SwapInput'
+
+const networks = [{ name: 'Ethereum' }, { name: 'Polygon' }]
+var sampleUserWallets: Wallet[] = [{ name: 'Wallet A' }, { name: 'Wallet B' }]
 
 const Navbar = () => {
-  const { systemTheme, theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
   return (
-    <div className="w-full bg-button-blue dark:bg-transparent">
-      <div className="flex px-5 py-3">
-        <div className="relative mr-2 h-16 w-16 ease-in-out hover:animate-spin-once">
-          <Link href="/">
-            <Image src="/logo.png" layout="fill" objectFit="contain" />
+    <div className="w-full bg-button-blue text-white dark:bg-transparent">
+      <div className="flex justify-evenly">
+        <div className="relative  my-2 h-12 w-12 flex-none hover:animate-spin-once">
+          <div className="flex justify-center">
+            <Link href="/">
+              <Image src="/logo.png" layout="fill" objectFit="contain" />
+            </Link>
+          </div>
+        </div>
+        <div className="hidden items-center p-3 text-2xl md:flex">
+          CryptoSwap
+        </div>
+
+        <div className="flex grow items-center justify-center gap-5 text-[#7D7E90]">
+          <Link href="/swap">
+            <a className="hover:text-white">Trade</a>
+          </Link>
+          <Link href="/pool">
+            <a className="hover:text-white">Pool</a>
+          </Link>
+          <Link href="/docs">
+            <a className="hover:text-white">Whitepaper</a>
           </Link>
         </div>
 
-        <div className="mx-10 flex grow items-center justify-center gap-5 sm:justify-start">
-          <Link href="/swap">
-            <a className="hover:text-white dark:marker:selection:text-[#7D7E90]">
-              Trade
-            </a>
-          </Link>
-          <Link href="/pool">
-            <a className="hover:text-white dark:text-[#7D7E90]">Pool</a>
-          </Link>
-          <Link href="/docs">
-            <a className="hover:text-white dark:text-[#7D7E90]">Docs</a>
-          </Link>
-        </div>
-        <button
-          type="button"
-          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-          className="relative mr-2 flex h-16 w-16 items-center justify-center"
-        >
-          {theme === 'dark' ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className=" h-6 w-6 text-white hover:fill-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              stroke-width="2"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          ) : (
-            <svg
-              className="h-6 w-6 hover:fill-black dark:text-black"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+        <div className="fixed inset-x-0 bottom-0 flex w-auto flex-row-reverse items-center justify-between bg-[#24264C] py-2 px-3 sm:static sm:flex-row sm:bg-transparent">
+          <span className="flex w-full flex-row justify-end gap-2">
+            <NetworksDropDown networks={networks} />
+            <ConnectedWalletsDropDown wallets={sampleUserWallets} />
+          </span>
+          <button
+            type="button"
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+            className="relative flex h-6 w-6 items-center justify-center sm:mx-5"
+          >
+            {theme === 'dark' ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 hover:fill-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
                 stroke-width="2"
-                d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-              ></path>
-            </svg>
-          )}
-        </button>
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="h-6 w-6 hover:fill-white"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                ></path>
+              </svg>
+            )}
+          </button>
+        </div>
       </div>
     </div>
   )
