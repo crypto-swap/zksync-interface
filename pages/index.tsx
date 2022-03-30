0b4f9355527b8c32795b101603f9d31eefc20f39
Nathan Lou <natloudev@gmail.com>
2022-03-30 14:09:59 -0700

    finished row-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,14 +53,20 @@ const Home: NextPage = () => {
                   <span className='text-xs md:text-md text-center font-semibold text-[#EFEFEF]'>Connect Your Wallet</span>
                 </button>
               </li>
-              <li className="">
-                <span>
-                  Step 1: connect your wallet ... and you're done!
+              <li className="flex mt-[33.33%]">
+                <span className='font-bold text-xs sm:text-sm md:text-md lg:text-lg '>
+                  Step 1: <br></br>
+                  connect your wallet <br></br>
+                  ... and you're done!
                 </span>
               </li>
-              <li className="">
-                <span>
-                  xd
+              <li className="flex mt-[20%]">
+                <span className='font-bold text-xs sm:text-sm md:text-md lg:text-lg '>
+                  Now you can access <br></br>
+                  CryptoSwap's great <br></br>
+                  features on an ever <br></br>
+                  expanding list of <br></br>
+                  crypto networks! 
                 </span>
               </li>
               <li className="bg-[#363C5F]  rounded-3xl square flex flex-col items-center">
@@ -75,63 +81,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>  
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center ">
-        <h1 className="text-6xl font-bold">
-          <a className="text-blue-600" href="https://nextjs.org">
-            CryptoSwap!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
