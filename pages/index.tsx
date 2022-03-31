7a47b42d52a28e27f2b262635f67f147295b0edc
Nathan Lou <natloudev@gmail.com>
2022-03-30 19:30:04 -0700

    finished first stage of the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,13 +36,13 @@ const Home: NextPage = () => {
       <div className='lg:hidden'>&nbsp;</div>
 
       <div  className="relative flex-col items-center pt-14 lg:pt-20" >
+
         <div className='text-4xl text-center font-semibold leading-7'>
           <p className="leading-7">Swapping crypto</p>
           <br></br>
           <p className="leading-7">should be simple</p>
         </div>
 
-        <div className="">
           <div className="flex-1 max-w-4x1 mx-auto pt-10 px-4 lg:px-[10%] xl:px-[20%]">
             <ul className="grid grid-cols-2 md:grid-cols-4 gap-y-6 gap-x-4">
               <li className="bg-[#363C5F]  rounded-3xl square flex flex-col items-center">
@@ -53,11 +53,15 @@ const Home: NextPage = () => {
                   <span className='text-xs md:text-md text-center font-semibold text-[#EFEFEF]'>Connect Your Wallet</span>
                 </button>
               </li>
-              <li className="flex mt-[33.33%]">
+              <li className="flex mt-[16.6666666%]">
                 <span className='font-bold text-md lg:text-lg '>
                   Step 1: <br></br>
                   connect your wallet <br></br>
-                  ... and you're done!
+                  ... and you're done! <br></br><br></br>
+                  <span className="text-xs leading-[0px]">
+                    <p className="leading-[0px]">No wallet?</p>
+                    <a href="" className="text-[#7408F8]">Click Here</a>
+                  </span>
                 </span>
               </li>
               <li className="flex mt-[20%]">
@@ -79,10 +83,64 @@ const Home: NextPage = () => {
               </li>
             </ul>
           </div>
+
+
+        <div className='text-xl font-bold leading-5 flex flex-col items-center justify-center pt-32'> 
+          <span>
+            <p className="leading-5">CryptoSwap has two features that</p>
+            <br></br>
+            <p className="leading-5">are being researched, tested, and </p>
+            <br></br>
+            <p className="leading-5">audited for algorithmic excellence. </p>
+            </span>      
         </div>
+
+          <div className="flex-1 max-w-4x1 mx-auto pt-10 px-4 lg:px-[10%] xl:px-[20%]">
+            <ul className="grid grid-cols-2 md:grid-cols-4 gap-y-6 gap-x-4">
+              <li className="bg-[#71B176]  rounded-3xl square flex flex-col items-center">
+                <button className="flex flex-col items-center square object-contain justify-center p-[10%] gap-y-2">
+                  <div className="h-auto">
+                    <img src={'/swap.svg'}/>
+                  </div>
+                  <span className='text-xs md:text-md text-center font-semibold text-[#EFEFEF]'>Swap</span>
+                </button>
+              </li>
+              <li className="flex mt-[20%]">
+                <span className='font-bold text-sm lg:text-lg '>
+                  Swap:<br></br>
+                  Select your wallets<br></br>
+                  and customize your<br></br>
+                  trade, CryptoSwap <br></br>
+                  will handle the rest.<br></br>
+                </span>
+              </li>
+              <li className="flex mt-[14.2857%]">
+                <span className='font-bold text-[13px] md:text-sm lg:text-md xl:text-lg '>
+                  Pool:<br></br>
+                  Pool your crypto funds <br></br>
+                  and collect a 0.25% fee <br></br>
+                  per trade. Your pooled <br></br>
+                  funds will be linked <br></br>
+                  to your wallet via the <br></br>
+                  CryptoSwap Protocol. <br></br>
+                </span>
+              </li>
+              <li className="bg-[#4B86FF]  rounded-3xl square flex flex-col items-center">
+                <button className="flex flex-col items-center square object-contain justify-center p-[10%] gap-y-2">
+                  <div className="h-auto">
+                    <img src={'/pool.svg'}/>
+                  </div>
+                  <span className='text-xs md:text-md text-center font-semibold text-[#EFEFEF]'>Pool</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+
       </div>  
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
+      <div className="mt-96">&nbsp;</div>
+
+      <footer className="flex h-24 w-full items-center justify-center">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -90,7 +148,7 @@ const Home: NextPage = () => {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          <Image src="/logo.png" alt="CryptoSwap Logo" width={16} height={16} /> CryptoSwap
         </a>
       </footer>
     </div>
