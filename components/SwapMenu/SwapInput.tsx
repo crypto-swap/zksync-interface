a28f609a6157790f95b5a50d451fd0cfbf514ca5
ismellpillows <calvinluo8@gmail.com>
2022-04-03 15:23:32 -0700

    token select, flip button, amount fields

--- components/SwapMenu/SwapInput.tsx
+++ components/SwapMenu/SwapInput.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import WalletsDropdownProps from '../SwapWalletDropdown'
+import SwapTokenSelect from './SwapTokenSelect'
+import { Token } from './SwapTokenSelect'
+
+const digits = ['', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] as const
+type Digit = typeof digits[number]
+export const isDigit = (value: any): value is Digit => digits.includes(value)
 
 export interface Wallet {
   name: string
@@ -9,10 +15,19 @@ var sampleUserWallets: Wallet[] = [{ name: 'Wallet A' }, { name: 'Wallet B' }]
 
 interface SwapInputProps {
   title: string
-  value: number
+  value: string
+  token: Token
+  setToken: React.Dispatch<React.SetStateAction<Token>>
 }
 
-const SwapInput = ({ title, value }: SwapInputProps): JSX.Element => {
+const SwapInput = ({
+  title,
+  value,
+  token,
+  setToken,
+}: SwapInputProps): JSX.Element => {
+  const [amount, setAmount] = React.useState(value)
+
   return (
     <div className="h-full p-2">
       <div className="flex">
@@ -20,8 +35,17 @@ const SwapInput = ({ title, value }: SwapInputProps): JSX.Element => {
         <WalletsDropdownProps wallets={sampleUserWallets} />
       </div>
 
-      <div className="rounded-lg  shadow-[inset_0.5px_1px_5px_rgba(0,0,0,0.3)]">
-        <div className="px-4 py-2">{value.toFixed(1)}</div>
+      <div className="relative flex flex-row-reverse place-content-between rounded-lg px-4 py-2 shadow-[inset_0.5px_1px_5px_rgba(0,0,0,0.3)]">
+        <input
+          value={amount}
+          onChange={(event) => {
+            if (/^\d*\.?\d*$/.test(event.target.value)) {
+              setAmount(event.target.value)
+            }
+          }}
+          className="absolute inset-0 inline-block h-full w-full rounded-lg bg-transparent p-4"
+        />
+        <SwapTokenSelect {...{ token, setToken }} />
       </div>
       <div className="p-1 text-xs font-light">Balance:</div>
     </div>
--- components/SwapMenu/index.tsx
+++ components/SwapMenu/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import SwapInput from './SwapInput'
 
 const SwapMenu = () => {
+  const [effect, setEffect] = useState(false)
+
+  const [fromToken, setFromToken] = useState('eth')
+  const [toToken, setToToken] = useState('bat')
+
   return (
-    <div className="bg-bg-card-light h-auto w-full max-w-md rounded-3xl shadow-card dark:bg-bg-card-dark dark:shadow-card-dark">
-      <div className="text-text-light h-full px-3 py-4 dark:text-text-dark">
+    <div className="h-auto w-full max-w-md rounded-3xl bg-bg-card-light shadow-card dark:bg-bg-card-dark dark:shadow-card-dark">
+      <div className="h-full px-3 py-4 text-text-light dark:text-text-dark">
         <div className="font-bold">Swap</div>
-        <SwapInput title="From" value={0.0} />
+        <SwapInput
+          title="From"
+          value={'0.0'}
+          token={fromToken}
+          setToken={setFromToken}
+        />
         <div className="flex justify-center">
-          <button className="">
-            <Image src={'/SwapButton.svg'} height={30} width={30} />
+          <button
+            className=""
+            onClick={() => {
+              setEffect(true)
+              const temp = fromToken
+              setFromToken(toToken)
+              setToToken(temp)
+            }}
+          >
+            <Image
+              src={'/SwapButton.svg'}
+              className={effect ? 'animate-spin-once' : ''}
+              onAnimationEnd={() => setEffect(false)}
+              height={30}
+              width={30}
+            />
           </button>
         </div>
-        <SwapInput title="To" value={0.0} />
+        <SwapInput
+          title="To"
+          value={'0.0'}
+          token={toToken}
+          setToken={setToToken}
+        />
       </div>
     </div>
   )
