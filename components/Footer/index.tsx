0b9722fb417635c1f35a0c4a46660bd6fb534146
Lil Tig <33049875+liltig@users.noreply.github.com>
2022-04-06 20:44:42 -0700

    updated footer with "join our community" and media links

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,27 +7,39 @@ const Footer = () => {
   const {theme} = useTheme();
   return (
     <div className="h-45 grid w-full grid-cols-1 grid-rows-4 justify-items-center lg:grid-cols-4 lg:grid-rows-1">
-      <div className="p-12 space-y-3">
-        <div className="grid grid-cols-5 gap-2 ">
-          <Link href="/swap">
-            <Image src={'/telegram.svg'} width={45} height={45} />
+      <div className="px-12 space-y-3">
+        <p className="text-xl font-semibold text-center">Join Our Community</p>
+        <div className="grid grid-cols-5 gap-2 pt-5 pb-3">
+          <Link href="https://t.me/+caEx-GqJJy5kMTEx">
+            <a target="_blank">
+              <Image src={'/telegram.svg'} width={45} height={45} />
+            </a> 
           </Link>
-          <Link href="">
-            <Image src={theme === 'dark' ? '/medium_dark.svg' : '/medium.svg'} width={45} height={45} />
+          <Link href="https://medium.com/@cryptoswaporg">
+            <a target="_blank">
+              <Image src={theme === 'dark' ? '/medium_dark.svg' : '/medium.svg'} width={45} height={45} />
+            </a>
           </Link>
-          <Link href="">
-            <Image src={'/discord.svg'} width={45} height={45} />
+          <Link href="https://discord.gg/KK2Gha9Cyb">
+            <a target="_blank">
+              <Image src={'/discord.svg'} width={45} height={45} />
+            </a>
           </Link>
-          <Link href="">
-            <Image src={'/twitter.svg'} width={45} height={45} />
+          <Link href="https://twitter.com/cryptoswaporg/">
+            <a target="_blank">
+              <Image src={'/twitter.svg'} width={45} height={45} />
+            </a>
+            
           </Link>
-          <Link href="">
+          <Link href="https://github.com/crypto-swap">
+            <a target="_blank">
             <Image src={theme === 'dark' ? 'github_dark.svg' : '/github.svg'} width={45} height={45} />
+            </a>
           </Link>
         </div>
         <p className="w-full text-center font-medium">© CryptoSwap 2022</p>
       </div>
-      <div className="space-y-3">
+      <div className="space-y-3 pt-6 py-3 lg:py-0 lg:pt-0">
         <h1 className="text-xl font-semibold"> About Us </h1>
         <div className="w-full space-y-2.5 text-center text-base font-semibold text-font-blue">
           <p>
@@ -47,7 +59,7 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="space-y-3">
+      <div className="space-y-3 py-3 lg:py-0">
         <h1 className="text-xl font-semibold"> Resources </h1>
         <div className="w-full text-center space-y-2.5 text-base font-semibold text-font-blue">
           <p>
@@ -67,7 +79,7 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="space-y-3">
+      <div className="space-y-3 py-3 lg:py-0">
         <h1 className="text-xl font-semibold"> Business </h1>
         <div className="w-full text-center space-y-2.5 text-base font-semibold text-font-blue">
           <p>
@@ -78,7 +90,7 @@ const Footer = () => {
         </div>
       </div>
       <br></br>
-      <div className="mt-4 ">&nbsp;</div>
+      <div className="mt-0 lg:mt-20 xl:mt-20 2xl:mt-16">&nbsp;</div>
     </div>
   );
 };
