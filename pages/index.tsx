95dd56930d7dbd8019c914167065814b76dc1d0a
Nathan Lou <natloudev@gmail.com>
2022-04-10 16:03:04 -0700

    various bugfixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,10 @@ const Home: NextPage = () => {
     <div className="text-text-light bg-bg-light h-screen font-[Montserrat] overflow-x-hidden dark:bg-bg-dark dark:text-text-dark">
       <Head>
         <title>CryptoSwap</title>
-        <link rel="icon" href="/black_logo.svg" />
+        <meta name="description" content="CryptoSwap" />
+        <meta key="twitter:description" name="twitter:description" content="CryptoSwap" />
+        <meta key="og:description" property="og:description" content="CryptoSwap" />
+        <link rel="icon" href="/icons/black_logo.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
