67c63cc79c298829af985f5cbe898d7aff3e068d
Lil Tig <33049875+liltig@users.noreply.github.com>
2022-04-15 20:18:51 -0700

    added mock pools

--- components/Pool/PoolMenu.tsx
+++ components/Pool/PoolMenu.tsx
@@ -1,13 +1,20 @@
 import PoolBar from './PoolBar'
+import pools from './mockPools.json';
 
 const PoolMenu = () => {
+    //temp images
+    var imgs = {
+        'SEK' : '/icons/discord.svg'
+
+    }
     return ( 
         <div>
             {
-                //testing parameters
+                pools.map((pool) => (
+                    
+                    <PoolBar image1='/icons/discord.svg' image2='/icons/discord.svg' ticker1={pool['asset1']} ticker2={pool['asset2']} liquidity={pool['liquidity']} volume={pool['volume']} fees={(pool['volume'] * 0.025).toFixed(2)}  apr={(pool['volume'] * 0.025 / pool['liquidity'] * 365).toFixed(5)} />
+                ))
             }
-            <PoolBar name="USD-BIT" liquidity="$500" volume="$200" fees="$0.01" apr="0.25%"/>
-            <PoolBar name="USD-BIT" liquidity="$500" volume="$200" fees="$0.01" apr="0.25%"/>
         </div>
     ) }
     
