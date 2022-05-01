fbc48be57098e1cf6564c0137adff391bbc9da68
Nathan Lou <natloudev@gmail.com>
2022-05-01 14:53:25 -0700

    got wallet to connect without context changing

--- src/context/wallet.ts
+++ src/context/wallet.ts
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { ethers } from 'ethers'
 import { useRouter } from 'next/router'
 
+let eth; 
+
 if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
+    eth = window.ethereum;
 }
