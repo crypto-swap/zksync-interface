46f2ee14689c7cf0147c667e0919f7c6a4afa604
Nathan Lou <natloudev@gmail.com>
2022-05-01 23:41:51 -0700

    wallet context setup

--- src/context/wallet.ts
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
-import { useRouter } from 'next/router'
-
-let eth; 
-
-if (typeof window.ethereum !== 'undefined') {
-    eth = window.ethereum;
-}
