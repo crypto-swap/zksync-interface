c3df459234edd9fab721d6f5acecc27d1854d360
ismellpillows <calvinluo8@gmail.com>
2022-05-06 22:15:05 -0700

    cleanup

--- src/components/Navbar/WalletPopup.tsx
+++ src/components/Navbar/WalletPopup.tsx
@@ -25,34 +25,36 @@ const WalletPopup = ({ setNetwork }: WalletPopupProps) => {
   }
 
   const active = useIsActive();
-  // const activating = useIsActivating();
   const [activating, setActivating] = useState(false);
   const error = useError();
 
   useEffect(() => {
     setActivating(false);
     if (active) {
+      setNetwork(0);
       closeModal();
     }
   }, [active, error]);
 
   useEffect(() => {
-    void metaMask.connectEagerly()
+    metaMask.connectEagerly()
   }, [])
+
+
+  const disabled = active || activating;
 
   return (
     <Popup title="Add Wallet" {...{ open, closeModal }}>
       <div className="mt-2.5">
         <button
-          disabled={active || activating}
+          {...{ disabled }}
           onClick={
             activating ? undefined : () => {
               setActivating(true);
-              setNetwork(0);
               metaMask.activate();
             }
           }
-          className={`mt-5 flex w-full items-center gap-3 rounded-lg bg-slate-500 bg-opacity-0 p-3 text-lg font-bold shadow-card ${active || activating ? '' : "hover:shadow-button-hover dark:hover:shadow-button-hover-dark"} dark:border-bg-light dark:shadow-card-dark `}
+          className={`mt-5 flex w-full items-center gap-3 rounded-lg bg-slate-500 bg-opacity-0 p-3 text-lg font-bold shadow-card ${disabled ? '' : "hover:shadow-button-hover dark:hover:shadow-button-hover-dark"} dark:border-bg-light dark:shadow-card-dark `}
         >
           <Image src={'/icons/metamask.svg'} width={30} height={30} />
           {active ?
