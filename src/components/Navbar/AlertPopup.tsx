1ac6e6493348a60a716b7acc9350605171ddd5b3
ismellpillows <calvinluo8@gmail.com>
2022-05-17 23:35:17 -0700

    alert popup when connect to wrong network

---
+++ src/components/Navbar/AlertPopup.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Popup from '../Navbar/Popup';
+import { ChevronRightIcon } from '@heroicons/react/solid';
+
+interface AlertPopupProps {
+  alertPopupOpen: boolean;
+  setAlertPopupOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AlertPopup = ({
+  alertPopupOpen: open,
+  setAlertPopupOpen: setOpen,
+}: AlertPopupProps) => {
+
+  function closeModal() {
+    setOpen(false);
+  }
+
+  return (
+    <Popup title="Unsupported Network" {...{ open: true, closeModal }}>
+      <p className="text-sm mt-4">
+        Wrong Network, please switch to zkSync Alpha Testnet.
+      </p>
+      <button
+        className="mt-4 w-full place-content-center flex rounded-lg p-3 text-lg shadow-card hover:shadow-button-hover dark:hover:shadow-button-hover-dark dark:border-bg-light dark:shadow-card-dark">
+        Switch Network
+        <ChevronRightIcon className='inline w-[30px] h-[30px]' />
+      </button>
+    </Popup>
+  );
+};
+
+export default AlertPopup;
