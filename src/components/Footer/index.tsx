64b697caf5e5670b700630d5b1ecac6d848e8e97
Nathan Lou <natloudev@gmail.com>
2022-05-28 15:49:53 -0700

    footer changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,23 +3,40 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useTheme } from 'next-themes';
 
-const Footer = () => {
-  const { theme } = useTheme();
-  return (
-    <div className="h-45 bg-bg-light grid w-full grid-cols-1 grid-rows-4 justify-items-center lg:grid-cols-4 lg:grid-rows-1 dark:bg-bg-dark">
-      <div className="px-12 space-y-3">
-        <p className="text-xl font-semibold text-center">Join Our Community</p>
-        <div className="grid grid-cols-5 gap-2 pt-5 pb-3">
-          <Link href="https://t.me/+caEx-GqJJy5kMTEx">
-            <a target="_blank">
-              <Image src={'/icons/telegram.svg'} width={45} height={45} />
-            </a>
-          </Link>
+
+/*
           <Link href="https://medium.com/@cryptoswaporg">
             <a target="_blank">
               <Image src={theme === 'dark' ? '/icons/medium_dark.svg' : '/icons/medium.svg'} width={45} height={45} />
             </a>
           </Link>
+          <Link href="https://t.me/+caEx-GqJJy5kMTEx">
+            <a target="_blank">
+              <Image src={'/icons/telegram.svg'} width={45} height={45} />
+            </a>
+          </Link>
+
+          <p>
+            <Link href="">
+              <a>Investors</a>
+            </Link>
+          </p>
+          <p>
+            <Link href="">
+              <a>Auditors</a>
+            </Link>
+          </p>
+*/
+
+const Footer = () => {
+  const { theme } = useTheme();
+  return (
+    <div className="h-45 bg-bg-light grid w-full grid-cols-1 grid-rows-4 justify-items-center lg:grid-cols-4 lg:grid-rows-1 dark:bg-bg-dark">
+      <div className="px-12 space-y-3">
+        <p className="text-xl font-semibold text-center pl-[10px]">Join Our Community</p>
+        <div className="grid grid-cols-3 pt-5 pl-[24px] pb-3">
+
+
           <Link href="https://discord.gg/KK2Gha9Cyb">
             <a target="_blank">
               <Image src={'/icons/discord.svg'} width={45} height={45} />
@@ -37,7 +54,7 @@ const Footer = () => {
             </a>
           </Link>
         </div>
-        <p className="w-full text-center font-medium">© CryptoSwap 2022</p>
+        <p className="w-full text-center  font-medium">© CryptoSwap 2022</p>
       </div>
       <div className="space-y-3 pt-6 py-3 lg:py-0 lg:pt-0">
         <div className="text-xl font-semibold"> About Us </div>
@@ -45,16 +62,6 @@ const Footer = () => {
           <p>
             <Link href="/team">
               <a>Team</a>
-            </Link>
-          </p>
-          <p>
-            <Link href="">
-              <a>Investors</a>
-            </Link>
-          </p>
-          <p>
-            <Link href="">
-              <a>Auditors</a>
             </Link>
           </p>
         </div>
@@ -73,7 +80,7 @@ const Footer = () => {
             </Link>
           </p>
           <p>
-            <Link href="#tutorial">
+            <Link href="https://cryptoswap.org/#tutorial">
               <a>Tutorials</a>
             </Link>
           </p>
