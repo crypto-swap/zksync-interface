f7b347fcd26b9e28b214699fbb337fa57a44e097
Nathan Lou <natloudev@gmail.com>
2022-05-28 06:45:18 -0700

    add pool templating

--- src/components/Add/AddPool.tsx
+++ src/components/Add/AddPool.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { ChevronLeftIcon, AdjustmentsIcon } from '@heroicons/react/solid'
+import { ChevronLeftIcon, AdjustmentsIcon, PlusIcon } from '@heroicons/react/solid'
 
 const style = {
     wrapper: `flex flex-col w-full max-w-5xl mr-auto ml-auto items-center flex-1 basis-0 overflow-hidden overflow-auto justify-center p-2`,
     returnBar: `flex justify-between items-center p-2 w-full bg-bg-card-light dark:bg-bg-card-dark shadow-card dark:shadow-card-dark rounded-2xl mx-2 mt-6 mb-4`,
     poolContainer: `auto-rows-auto max-w-5xl w-full flex flex-col md:flex-row justify-start md:justify-between`,
+    poolInfo: `flex flex-1 flex-row md:mr-4 justify-start rounded-2xl bg-bg-card-light dark:bg-bg-card-dark shadow-card dark:shadow-card-dark`,
+    addPoolMenu: `w-full md:w-96 mt-4 md:mt-0 rounded-2xl shrink-0 p-[20px] bg-bg-card-light dark:bg-bg-card-dark shadow-card dark:shadow-card-dark`,
 
 }
 
@@ -26,13 +28,13 @@ const AddPool = () => {
                 </div>
             </div>
             <div className={style.poolContainer}>
-                <div className="flex flex-1 flex-row md:mr-4 justify-start rounded-2xl bg-bg-card-light dark:bg-bg-card-dark shadow-card dark:shadow-card-dark">
+                <div className={style.poolInfo}>
                     <div className="flex flex-right flex-row justify-start p-8 mr-4">
                         test
                     </div>
 
                 </div>
-                <div className="w-full md:w-96 mt-4 md:mt-0 rounded-2xl shrink-0 p-[20px] bg-bg-card-light dark:bg-bg-card-dark shadow-card dark:shadow-card-dark">
+                <div className={style.addPoolMenu}>
                     <div className="box-border w-full flex flex wrap items-center min-w-0">
                         <div className="border-box w-full flex items-center justify-between">
                             <div className="flex flex-nowrap flex-row justify-start items-center">
@@ -46,6 +48,13 @@ const AddPool = () => {
                                     <AdjustmentsIcon className="w-6 h-6" />
                                 </div></button>
                             </div>
+                        </div>
+                    </div>
+                    <div className="mt-[20px]">
+                        <div className="grid auto-rows-auto gap-y-[20px] mt-[0.2rem]">
+                            <div id="add-liquidity-input-token-a" className=""></div>
+                            <div className="flex flex-col justify-start items-center w-full"><PlusIcon className="w-5 h-5" /></div>
+                            <div id="add-liquidity-input-token-b" className=""></div>
                         </div>
                     </div>
                 </div>
