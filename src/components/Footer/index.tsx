18794a54f45d154f04c4efd8fc1f8c8315aff9b0
Nathan Lou <natloudev@gmail.com>
2022-05-28 19:40:55 -0700

    finished twitter

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
   const { theme } = useTheme();
   return (
     <div className="h-45 bg-bg-light grid w-full grid-cols-1 grid-rows-4 justify-items-center lg:grid-cols-4 lg:grid-rows-1 dark:bg-bg-dark">
-      <div className="px-12 space-y-3">
+      <div className="px-12 space-y-0">
         <p className="text-xl font-semibold text-center pl-[10px]">Join Our Community</p>
         <div className="grid grid-cols-3 pt-5 pl-[24px] pb-3">
 
@@ -58,16 +58,21 @@ const Footer = () => {
       </div>
       <div className="space-y-3 pt-6 py-3 lg:py-0 lg:pt-0">
         <div className="text-xl font-semibold"> About Us </div>
-        <div className="w-full space-y-2.5 text-center text-base font-semibold text-font-blue">
+        <div className="w-full space-y-2 text-center text-base font-semibold text-font-blue">
           <p>
             <Link href="/team">
               <a>Team</a>
             </Link>
           </p>
+          <p>
+            <Link href="">
+              <a>Auditors</a>
+            </Link>
+          </p>
         </div>
       </div>
-      <div className="space-y-3 py-3 lg:py-0">
-        <div className="text-xl font-semibold"> Resources </div>
+      <div className="space-y-3 lg:py-0">
+        <div className="text-xl font-semibold flex justify-center"> Resources </div>
         <div className="w-full text-center space-y-2.5 text-base font-semibold text-font-blue">
           <p>
             <Link href="/whitepaper.pdf">
