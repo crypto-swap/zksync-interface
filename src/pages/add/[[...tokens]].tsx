dd0faa9a3890599cf78d8f0cc553c63cfad193b2
Nathan Lou <natloudev@gmail.com>
2022-05-30 23:26:54 -0700

    templated slug

--- src/pages/add/[[...tokens]].tsx
+++ src/pages/add/[[...tokens]].tsx
@@ -4,6 +4,7 @@ import Footer from '../../components/Footer';
 import React, { useState, useCallback, useContext, useEffect, } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { ChevronLeftIcon, AdjustmentsIcon, PlusIcon } from '@heroicons/react/solid';
 import PoolInput from '../../components/Add/PoolInput';
 import { hooks } from '../../connectors/metaMask';
@@ -92,6 +93,9 @@ function convert(
 
 
 const Add: React.FC = (): JSX.Element => {
+
+    const router = useRouter()
+    const add = router.query.add || []
 
     const params = useParams(); // views the parameters from routing i.e add/ETH/BTC <--
 
