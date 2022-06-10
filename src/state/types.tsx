69440f32db0eecfbfc019aaf75cbb6f415708f1a
Nathan Lou <natloudev@gmail.com>
2022-06-10 14:10:59 -0700

    uniswap tokenlist

--- src/state/types.tsx
+++ src/state/types.tsx
@@ -2,10 +2,9 @@ import BigNumber from 'bignumber.js'
 import { BigNumber as EthersBigNumber } from '@ethersproject/bignumber'
 import { Token, ChainId } from '@crypto-swap/sdk'
 import { parseUnits } from '@ethersproject/units'
-
+import { TokenInfo, TokenList, Tags } from '@uniswap/token-lists'
 
 export type TokenAddressMap = Readonly<{
-    [chainId in ChainId]: Readonly<{ [tokenAddress: string]: { list: "zkSync alpha testnet" } }>
+    [chainId in ChainId]: Readonly<{ [tokenAddress: string]: { list: TokenList } }>
 }>
 
-// instead of zkSync alphatestnet we would want uniswap tokenlist
