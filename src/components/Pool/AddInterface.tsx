bbb98698d3998250dae9b13aaccea40bdde75c50
natlou <natloudev@gmail.com>
2022-07-12 16:02:11 -0700

    removed html from Pool and transfered to AddInterface

--- src/components/Pool/AddInterface.tsx
+++ src/components/Pool/AddInterface.tsx
@@ -1,8 +1,39 @@
+import PoolInput from './PoolInput';
+import AddPoolButton from './AddPoolButton';
 
 const AddInterface = () => {
     
     return (
         <>
+                    <div className="grid auto-rows-auto gap-y-[20px] mt-[0.2rem]">
+              <div id="add-liquidity-input-token-a" className="">
+                <PoolInput {...{
+                  value: tokenA_Amount,
+                  token: tokenA,
+                  setToken: setTokenA,
+                  setTokenA_Amount: setTokenA_Amount,
+                  setTokenB_Amount: setTokenB_Amount,
+                }}
+                  onChange={handleChange} />
+              </div>
+              <div className="flex flex-col justify-start items-center w-full"><PlusIcon className="w-5 h-5" /></div>
+              <div id="add-liquidity-input-token-b" className="">
+                <PoolInput
+                  tokenB
+                  {...{
+                    value: tokenB_Amount,
+                    token: tokenB,
+                    setToken: setTokenB,
+                    setTokenA_Amount: setTokenA_Amount,
+                    setTokenB_Amount: setTokenB_Amount,
+                  }}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="mt-12">
+              <AddPoolButton />
+            </div>
         </>
     )
 }
--- src/components/Pool/Pool.tsx
+++ src/components/Pool/Pool.tsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { ChevronLeftIcon, AdjustmentsIcon, PlusIcon } from '@heroicons/react/solid';
-import PoolInput from './PoolInput';
-import AddPoolButton from './AddPoolButton';
+
 import RemoveInterface from './RemoveInterface';
 import AddInterface from './AddInterface';
 
@@ -173,35 +172,7 @@ const AddPool = () => {
             </div>
           </div>
           <div className="mt-9">
-            <div className="grid auto-rows-auto gap-y-[20px] mt-[0.2rem]">
-              <div id="add-liquidity-input-token-a" className="">
-                <PoolInput {...{
-                  value: tokenA_Amount,
-                  token: tokenA,
-                  setToken: setTokenA,
-                  setTokenA_Amount: setTokenA_Amount,
-                  setTokenB_Amount: setTokenB_Amount,
-                }}
-                  onChange={handleChange} />
-              </div>
-              <div className="flex flex-col justify-start items-center w-full"><PlusIcon className="w-5 h-5" /></div>
-              <div id="add-liquidity-input-token-b" className="">
-                <PoolInput
-                  tokenB
-                  {...{
-                    value: tokenB_Amount,
-                    token: tokenB,
-                    setToken: setTokenB,
-                    setTokenA_Amount: setTokenA_Amount,
-                    setTokenB_Amount: setTokenB_Amount,
-                  }}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <div className="mt-12">
-              <AddPoolButton />
-            </div>
+
           </div>
         </div>
       </div>
