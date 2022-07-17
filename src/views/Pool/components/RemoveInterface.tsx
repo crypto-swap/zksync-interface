d9743dc7db4342f083c7604fec41db11022c5313
natlou <natloudev@gmail.com>
2022-07-16 21:38:11 -0700

    templated styling for remove output

--- src/views/Pool/components/RemoveInterface.tsx
+++ src/views/Pool/components/RemoveInterface.tsx
@@ -1,16 +1,19 @@
 import RemoveLiquidityButton from './RemoveLiquidityButton';
 import RemoveInput from './RemoveInput';
 import RemoveOutput from './RemoveOutput';
-import { REFUSED } from 'dns';
+import { ArrowDownIcon } from '@heroicons/react/solid';
 
 const style = { 
-    wrapper: ``,
+    wrapper: `pb-3`,
+    arrowContainer: `pt-2 pb-1 flex flex-col justify-center items-center`,
 }
 
 const RemoveInterface = () => {
     return (
         <div className={style.wrapper}>
             <RemoveInput/>
+            <div className={style.arrowContainer}><ArrowDownIcon className="w-5 h-5"/></div>
+            <RemoveOutput/>
             <RemoveLiquidityButton/>
         </div>
     )
