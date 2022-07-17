43933bb47883e867e22bcb5999ccbdaad225a52d
natlou <natloudev@gmail.com>
2022-07-16 22:07:49 -0700

    2 tokens output

--- src/views/Pool/components/RemoveOutput.tsx
+++ src/views/Pool/components/RemoveOutput.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 const style = {
     wrapper : `pb-12`,
     header : `pb-2 flex place-content-between items-center px-0.5`,
-    inputContainer: `relative flex flex-row-reverse place-content-between text-lg h-[60px] rounded-lg px-4 py-2 shadow-[inset_0.5px_1px_5px_rgba(0,0,0,0.3)]`,
-    inputBox: `absolute inset-0 h-full w-full rounded-lg bg-transparent p-4`,
+    outputBox: `flex flex-col h-full w-full rounded-lg bg-transparent p-4 shadow-[inset_0.5px_1px_5px_rgba(0,0,0,0.3)]`,
+    asset1: `text-lg`,
+    asset2: `text-lg pt-2`
 }
 
 const RemoveOutput = () => {
@@ -14,12 +15,13 @@ const RemoveOutput = () => {
             <div className={style.header}>
                 <div className="text-sm font-bold text-gray-500 dark:text-gray-400 ">You'll receive</div>
             </div>
-            <div className={style.inputContainer}>
-                <input 
-                    inputMode="decimal"
-                    placeholder="0.0"
-                    className={style.inputBox} 
-                />
+            <div className={style.outputBox}>
+                <div className={style.asset1}>
+                    0.0
+                </div>
+                <div className={style.asset2}>
+                    0.0
+                </div>
             </div>
         </div>
     )
