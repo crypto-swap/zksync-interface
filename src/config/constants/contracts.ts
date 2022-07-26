a6a37afd83d6a35b41bb7786c5d21c8667d967d0
natlou <natloudev@gmail.com>
2022-07-25 21:40:35 -0700

    added rpc url

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,3 @@
+const testnetFaucet = '0x1458a212860e756f9E3D4b016db037a054EA4C5F';
+
+export default testnetFaucet;
---
+++ src/config/constants/rpc.ts
@@ -0,0 +1,3 @@
+const ZKSYNC_TESTNET_RPC_URL = 'https://zksync2-testnet.zksync.dev';
+
+export default ZKSYNC_TESTNET_RPC_URL;
