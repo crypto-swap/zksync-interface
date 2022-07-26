6040deef7d84b0a6a4ea1c174c9496736f9f4c3e
natlou <natloudev@gmail.com>
2022-07-25 21:50:59 -0700

    synswap disclaimer

--- src/views/Faucet/index.tsx
+++ src/views/Faucet/index.tsx
@@ -6,6 +6,7 @@ const style = {
     header : `mb-[27px] text-xl font-bold text-left`,
     desc : `mb-[27px]`,
     zkSyncText : `text-[#8C8DFC] cursor-pointer font-semibold`,
+    syncSwapText : `text-[#8C8DFC] font-semibold`,
     button : `flex select-none items-center justify-center bg-button-blue  leading-none font-normal 
     cursor-pointer text-white h-[55px] text-lg leading-[24px] rounded-xl w-full shadow-blue-button hover:shadow-blue-button-hover`,
 }
@@ -20,6 +21,9 @@ const FaucetMenu = () => {
                 You can request tokens from our faucet for testing, once per account.
                 <br></br><br></br>
                 Please claim some tokens from <Link href="https://portal.zksync.io/faucet"><span className={style.zkSyncText}>the zkSync Portal</span></Link> first for gas.
+                <br></br><br></br>
+                Note: Since we are collaborating with <span className={style.syncSwapText}>SyncSwap</span> to implement this faucet, 
+                any funds claimed from the <span className={style.syncSwapText}>SyncSwap faucet</span> will not be claimable. 
             </div>
             <button className={style.button}>
                 <span className="font-semibold">Claim Testnet Tokens</span>
