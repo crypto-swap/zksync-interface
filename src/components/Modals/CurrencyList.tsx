dc4beb71cd460402e0c2f31eefedcde9b93ac3f5
natlou <natloudev@gmail.com>
2022-07-31 00:07:35 -0700

    tokenlist integration part1

--- src/components/Modals/CurrencyList.tsx
+++ src/components/Modals/CurrencyList.tsx
@@ -2,6 +2,7 @@ import React, {FC, useCallback, useMemo } from 'react';
 import { Currency } from '@crypto-swap/sdk'
 import { useAccount } from '../../hooks'
 import { UnicodeNormalizationForm } from 'ethers/lib/utils';
+import TOKEN_LIST from '../../config/constants/testnet.tokenlist.json'
 
 
 const CurrencyRow = () => {
@@ -10,6 +11,11 @@ const CurrencyRow = () => {
 
     console.log(account)
 
+    for (let i = 0; i < TOKEN_LIST.tokens.length; i++) {
+        console.log(TOKEN_LIST.tokens[i].symbol)
+        console.log(TOKEN_LIST.tokens[i].address)
+    }
+    
     return (
         <>
         </>
---
+++ src/hooks/Tokens.ts
@@ -0,0 +1,5 @@
+import TOKEN_LIST from '../config/constants/testnet.tokenlist.json'
+
+function useTokensFromMap(tokenMap) {
+    TOKEN_LIST.tokens[0]
+}
