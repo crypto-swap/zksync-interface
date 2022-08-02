28d536fdefbad329ca0f4ce77b828de5721633fb
natlou <natloudev@gmail.com>
2022-08-01 18:24:34 -0700

    finished initial useCurrencyBalance

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useActiveWeb3React } from './useActiveWeb3React';
 export { default as useAccount } from './useAccount';
 export { default as useProvider } from './useProvider';
+export { default as useCurrencyBalance } from './useCurrencyBalance';
 export * from './Token';
--- src/hooks/useCurrencyBalance.ts
+++ src/hooks/useCurrencyBalance.ts
@@ -5,15 +5,21 @@ import { useAllTokens } from '.';
 import TOKEN_LIST from '../config/constants/testnet.tokenlist.json'
 
 const tokenSymbols = useAllTokens
-
 const tokens = TOKEN_LIST.tokens
 
-const ERC20Interface = require("../config/abi/erc20.json");
+const ERC20ABI = require("../config/abi/erc20.json");
+const zksync = require('zksync-web3');
 
 const provider = useProvider();
 const account = useAccount();
 const signer = provider?.getSigner();
 
-function useCurrencyBalance(account?: string, token?: Token) {
+function useCurrencyBalance(account?: string, token?: string) {
+    
+    const tokenAddress = tokens[tokenSymbols.indexOf(token!)].address;
+    const tokenContract = new zksync.Contract(tokenAddress, ERC20ABI, signer);
+    console.log(tokenContract.balanceOf(account))
 
 }
+
+export default useCurrencyBalance
