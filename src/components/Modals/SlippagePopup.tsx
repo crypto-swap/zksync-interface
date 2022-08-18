c9e8b47bf397e55653c0bbdeddc5a54e33311bc2
natlou <natloudev@gmail.com>
2022-08-17 23:47:09 -0700

    templated slippage modal:

--- src/components/Modals/SlippagePopup.tsx
+++ src/components/Modals/SlippagePopup.tsx
@@ -1,8 +1,24 @@
+import React, {useState} from 'react';
+import Popup from './Popup';
 
 const SlippageModal = () => {
+
+    const [open, setOpen] = useState(false);
+    const [query, setQuery] = useState('');
+  
+    function closeModal() {
+      setOpen(false);
+    }
+  
+    function openModal() {
+      setQuery('');
+      setOpen(true);
+    }
+
     return (
-        <>
-        </>
+        <Popup title="Slippage" {...{ open, closeModal }}> 
+            Slippage Test
+        </Popup>
     )
 }
 
